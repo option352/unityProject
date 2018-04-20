db5bfa014e482bcb63995e6bda10b8ef83909aa6
option352 <keisuke.miyagawa@gmail.com>
2018-04-20 16:22:20 +0900

    ボタン追加

---
+++ Assets/Scripts/scoreChanger.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class scoreChanger : MonoBehaviour {
+
+	[SerializeField]
+	public Text scoreText;
+	public string tex;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void ChangeScore(int delta) {
+		if (scoreText) {
+			int current = int.Parse (scoreText.text);
+			scoreText.text = (current + delta).ToString();
+		}
+	}
+}
