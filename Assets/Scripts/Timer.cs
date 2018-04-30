290f8927cf11ba7c3495e32bd72bd074319a7508
option352 <keisuke.miyagawa@gmail.com>
2018-05-01 00:55:39 +0900

    経過時間表示

---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour {
+
+    float CountTimer = 0;
+
+	// Use this for initialization
+	void Start () {
+        CountTimer = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        CountTimer += Time.deltaTime;
+        GetComponent<Text>().text = CountTimer.ToString("F2");
+	}
+}
---
+++ Assets/Scripts/timerControl.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class timerControl {
+
+    /// <summary>
+    /// 経過時間
+    /// </summary>
+    public float CurrentTime
+    {
+        get;
+        private set;
+    }
+
+    /// <summary>
+    /// 残り時間
+    /// </summary>
+    public float RemainingTime
+    {
+        get
+        {
+            return LimitTime - CurrentTime;
+        }
+        private set { }
+    }
+
+    /// <summary>
+    /// 停止時間
+    /// </summary>
+    public float LimitTime
+    {
+        get;
+        set;
+    }
+
+    /// <summary>
+    /// 停止時間到達時に発火
+    /// </summary>
+    public Delegate.VoidDelegate LimitDelegate
+    {
+        get;
+        set;
+    }
+
+    bool isEnable = true;
+    public bool IsEnable
+    {
+        get
+        {
+            return isEnable;
+        }
+        set
+        {
+            isEnable = value;
+            if(!value)
+            {
+                CurrentTime = 0;
+            }
+        }
+    }
+
+    public bool Update()
+    {
+        if (IsEnable)
+        {
+            CurrentTime += Time.deltaTime;
+            if(CurrentTime >= LimitTime)
+            {
+                CurrentTime = 0;
+                if(LimitDelegate != null)
+                {
+                    LimitDelegate();
+                }
+                return true;
+            }
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+}
